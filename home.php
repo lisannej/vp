079132297f0dbb42fb33ae09dcbb022462743d5c
Lisanne Järv <lisannejarv@gmail.com>
2020-09-15 08:56:44 +0300

    Update home.php

--- home.php
+++ home.php
@@ -68,7 +68,7 @@ if ($semesterstart < $today && $today < $semesterend ) {
 
 </head>
 <body>
-  <img src="IMG/vp_banner.png" alt="Veebiprogrammeerimise kursuse banner">
+  <img src="../IMG/vp_banner.png" alt="Veebiprogrammeerimise kursuse banner">
   <h1><?php echo $username; ?></h1>
   <p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
   <p> See konkreetne leht on loodud veebiprogrammeerimise kursusel aasta 2020 sügissemestril <a href="https://www.tlu.ee">Tallinna Ülikooli </a> 
