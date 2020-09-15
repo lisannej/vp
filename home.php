a80d162434b8392f177bc3b5012415118c6ad7bb
Lisanne Järv <lisannejarv@gmail.com>
2020-09-15 11:45:46 +0300

    Update home.php

--- home.php
+++ home.php
@@ -116,14 +116,7 @@ for ($i = 0; $i < $piccount; $i ++) {
 }
 require ("header.php");
 ?>
-<!DOCTYPE html>
-<html lang="et">
-<head>
-  <meta charset="utf-8">
-  <title><?php echo $username; ?> programmeerib veebi</title>
 
-</head>
-<body>
   <img src="IMG/vp_banner.png" alt="Veebiprogrammeerimise kursuse banner">
   <h1><?php echo $username; ?></h1>
   <p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
