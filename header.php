8e14db48e42e5e035f15307d96ef10810fa190e4
Lisanne Järv <lisannej@users.noreply.github.com>
2020-09-21 21:00:48 +0300

    navigation

--- header.php
+++ header.php
@@ -3,6 +3,13 @@
 <head>
   <meta charset="utf-8">
   <title><?php echo $username; ?> programmeerib veebi</title>
-
 </head>
 <body>
+  <table>
+    <tr>
+      <td><a href="home.php">Kodu</a></td>
+      <td><a href="mote.php">Mote</a></td>
+      <td><a href="tabel.php">Tabel</a></td>
+    </tr>
+  </table>
+
