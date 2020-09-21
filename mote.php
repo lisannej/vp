ad58fdc64064c14b19a4ca0b34421ca8e4cfcc14
Lisanne Järv <lisannejarv@gmail.com>
2020-09-21 21:20:18 +0300

    Create mote.php

---
+++ mote.php
@@ -0,0 +1,12 @@
+<?php
+    require ("header.php");
+?>
+
+        <form method = "POST">
+        <label>Sisesta oma pahe tulnud mote!</label>
+        <input type="text" name="ideainput" placeholder="Kirjuta siia mote!">
+        <input type="submit" name="ideasubmit" value="Saada mote ara!"> 
+        </form>
+
+    </body>
+</html>
