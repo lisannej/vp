0c649e4588f69853a07f898c3cfc4bd6c2a3fc4f
Lisanne Järv <lisannejarv@gmail.com>
2020-09-21 21:44:48 +0300

    copied over php from home

--- home.php
+++ home.php
@@ -1,113 +1,97 @@
 <?php
-//var_dump ($_POST);
+  //var_dump ($_POST);
 
-//kui on idee sisestatud ja nuppu vajutatud, salvestame selle andmebaasi
-if(isset($_POST["ideasubmit"]) and !empty($_POST ["ideainput"])){
-  $conn = new mysqli ($serverhost, $serverusername, $serverpassword, $database );
-  //valmistan ette SQL kasu
-  $stmt = $conn->prepare ("INSERT INTO myideas (idea) VALUES (?) ");
-  echo $conn->error;
-  //seome kasuga meie parisandmed
-  //i - integer, d- decimal, s - string
-  $stmt->bind_param("s", $_POST ["ideainput"]);
-  $stmt->execute ();
-  $stmt->close ();
-  $conn->close ();
-} 
+  $username = "Lisanne Järv";
+  $daydate = date("d");
+  $yearnow = date ("Y");
+  $hournow = date("H");
+  $fulltimenow = date ("H.i.s");
+  $partofday = "lihtsalt aeg";
+  $weekdaynameset= ["esmaspäev", "teisipäev", "kolmapäev", "neljapäev",
+  "reede", "laupäev", "pühapäev"];
+  $monthnameset = ["jaanuar", "veebruar", "märts", "aprill", "mai",
+  "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
+  //echo $weekdaynamesET
+  //var_dump ($weekdaynameset);
+  $weekdaynow = date ("N");
+  //echo $weekdaynow ;
+  $monthnow = date ("F");
+  $monthnowint= (int) $monthnow;
 
-
-
-$username = "Lisanne Järv";
-$daydate = date("d");
-$yearnow = date ("Y");
-$hournow = date("H");
-$fulltimenow = date ("H.i.s");
-$partofday = "lihtsalt aeg";
-$weekdaynameset= ["esmaspäev", "teisipäev", "kolmapäev", "neljapäev",
- "reede", "laupäev", "pühapäev"];
-$monthnameset = ["jaanuar", "veebruar", "märts", "aprill", "mai",
- "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
-//echo $weekdaynamesET
-//var_dump ($weekdaynameset);
-$weekdaynow = date ("N");
-//echo $weekdaynow ;
-$monthnow = date ("F");
-$monthnowint= (int) $monthnow;
-
-if($hournow < 6){
-	$partofday = "uneaeg";
-} // enne 6
-if($hournow >= 6.01 and $hournow <= 9) {
-	$partofday = "hommik";
-}
-if($hournow > 9.01 and $hournow <=16) {
-	$partofday = "kooliaeg";
-} // 
-if($hournow > 16.01 and $hournow <= 18) {
-	$partofday = "õppimise aeg";
-} // oppimine
-if($hournow > 18.01 and $hournow <= 21) {
-	$partofday = "ohtu";
-} // ohtu
-if($hournow >21.01 and $hournow < 23.59){
-	$partofday = "mine magama";
-} // magama
-
-// vaatame semestri kulgemist 
-$semesterstart = new DateTime ("2020-8-31"); 
-$semesterend = new DateTime ("2020-12-13"); 
-$semesterduration = $semesterstart->diff($semesterend); 
-$semesterdurationdays = $semesterduration->format ("%r%a"); 
-$today = new DateTime ("now"); 
-$temp = $semesterstart->diff($today); 
-$daysnumber = $temp->days; 
-$semesterpercent = $daysnumber/$semesterdurationdays * 100 ; 
- $semesterpercent = round ($semesterpercent, 2);
-
-if ($semesterpercent < 0 ) { 
-   $semesterpercent = 0; 
-} 
-if ($semesterpercent > 100 ) { 
-  $semesterpercent = 1; 
-}
-if ($today < $semesterstart ) {
-  echo " Semester pole alanud" ;
-}
-if ($today > $semesterend) {
-  echo " Semester on läbi";
-}
-if ($semesterstart < $today && $today < $semesterend ) {
-  echo " Semester käib <br>";
-
-}
-//annan ette lubatud piltivormingute loendi
-$picfiletypes = ["image/jpeg", "image/png"];
-// loeme piltide kataloogi sisu ja naitame pilte
-$allfiles = array_slice (scandir ("vp_pics/"), 2);
-//var_dump ($allfiles);
-//$picfiles = array_slice ($allfiles, 2);
-$picfiles = [];
-//var_dump ($picfiles);
-foreach ($allfiles as $thing) {
-  $fileinfo = getImagesize ("vp_pics/" .$thing);
-  if (in_array($fileinfo["mime"], $picfiletypes) == true) {
-    array_push ($picfiles, $thing);
+  if($hournow < 6){
+    $partofday = "uneaeg";
+  } // enne 6
+  if($hournow >= 6.01 and $hournow <= 9) {
+    $partofday = "hommik";
   }
-}
+  if($hournow > 9.01 and $hournow <=16) {
+    $partofday = "kooliaeg";
+  } // 
+  if($hournow > 16.01 and $hournow <= 18) {
+    $partofday = "õppimise aeg";
+  } // oppimine
+  if($hournow > 18.01 and $hournow <= 21) {
+    $partofday = "ohtu";
+  } // ohtu
+  if($hournow >21.01 and $hournow < 23.59){
+    $partofday = "mine magama";
+  } // magama
 
-//paneme koik pildid ekraanile
-$piccount = count ($picfiles);
-//$i = $i + 1;
-// $i ++
-// $i +=2
-$imghtml = "";
-//<img src="IMG/failinimi.png" alt="text">
-//for ($i = 0; $i < $piccount; $i ++) {
-$i = mt_rand(0, ($piccount - 1));
-$imghtml .= '<img src="vp_pics/' .$picfiles[$i].'"';
-$imghtml .= 'alt="Tallinna Ulikool">';
-//}
-require ("header.php");
+  // vaatame semestri kulgemist 
+  $semesterstart = new DateTime ("2020-8-31"); 
+  $semesterend = new DateTime ("2020-12-13"); 
+  $semesterduration = $semesterstart->diff($semesterend); 
+  $semesterdurationdays = $semesterduration->format ("%r%a"); 
+  $today = new DateTime ("now"); 
+  $temp = $semesterstart->diff($today); 
+  $daysnumber = $temp->days; 
+  $semesterpercent = $daysnumber/$semesterdurationdays * 100 ; 
+  $semesterpercent = round ($semesterpercent, 2);
+
+  if ($semesterpercent < 0 ) { 
+    $semesterpercent = 0; 
+  } 
+  if ($semesterpercent > 100 ) { 
+    $semesterpercent = 1; 
+  }
+  if ($today < $semesterstart ) {
+    echo " Semester pole alanud" ;
+  }
+  if ($today > $semesterend) {
+    echo " Semester on läbi";
+  }
+  if ($semesterstart < $today && $today < $semesterend ) {
+    echo " Semester käib <br>";
+
+  }
+  //annan ette lubatud piltivormingute loendi
+  $picfiletypes = ["image/jpeg", "image/png"];
+  // loeme piltide kataloogi sisu ja naitame pilte
+  $allfiles = array_slice (scandir ("vp_pics/"), 2);
+  //var_dump ($allfiles);
+  //$picfiles = array_slice ($allfiles, 2);
+  $picfiles = [];
+  //var_dump ($picfiles);
+  foreach ($allfiles as $thing) {
+    $fileinfo = getImagesize ("vp_pics/" .$thing);
+    if (in_array($fileinfo["mime"], $picfiletypes) == true) {
+      array_push ($picfiles, $thing);
+    }
+  }
+
+  //paneme koik pildid ekraanile
+  $piccount = count ($picfiles);
+  //$i = $i + 1;
+  // $i ++
+  // $i +=2
+  $imghtml = "";
+  //<img src="IMG/failinimi.png" alt="text">
+  //for ($i = 0; $i < $piccount; $i ++) {
+  $i = mt_rand(0, ($piccount - 1));
+  $imghtml .= '<img src="vp_pics/' .$picfiles[$i].'"';
+  $imghtml .= 'alt="Tallinna Ulikool">';
+  //}
+  require ("header.php");
 ?>
 
   <img src="IMG/vp_banner.png" alt="Veebiprogrammeerimise kursuse banner">
@@ -122,12 +106,11 @@ require ("header.php");
   <?php echo $imghtml ; ?>
   
   <hr>
-  <form method = "POST">
-    <label>Sisesta oma pahe tulnud mote!</label>
-    <input type="text" name="ideainput" placeholder="Kirjuta siia mote!">
-    <input type="submit" name="ideasubmit" value="Saada mote ara!"> 
-  </form>
+    <form method = "POST">
+      <label>Sisesta oma pahe tulnud mote!</label>
+      <input type="text" name="ideainput" placeholder="Kirjuta siia mote!">
+      <input type="submit" name="ideasubmit" value="Saada mote ara!"> 
+    </form>
   <hr>
- 
 </body>
 </html>
--- mote.php
+++ mote.php
@@ -1,13 +1,23 @@
 <?php
     require ("header.php");
-
+    //kui on idee sisestatud ja nuppu vajutatud, salvestame selle andmebaasi
+    if(isset($_POST["ideasubmit"]) and !empty($_POST ["ideainput"])){
+        $conn = new mysqli ($serverhost, $serverusername, $serverpassword, $database );
+        //valmistan ette SQL kasu
+        $stmt = $conn->prepare ("INSERT INTO myideas (idea) VALUES (?) ");
+        echo $conn->error;
+        //seome kasuga meie parisandmed
+        //i - integer, d- decimal, s - string
+        $stmt->bind_param("s", $_POST ["ideainput"]);
+        $stmt->execute ();
+        $stmt->close ();
+        $conn->close ();
+    } 
 ?>
-
         <form method = "POST">
-        <label>Sisesta oma pahe tulnud mote!</label>
-        <input type="text" name="ideainput" placeholder="Kirjuta siia mote!">
-        <input type="submit" name="ideasubmit" value="Saada mote ara!"> 
+            <label>Sisesta oma pahe tulnud mote!</label>
+            <input type="text" name="ideainput" placeholder="Kirjuta siia mote!">
+            <input type="submit" name="ideasubmit" value="Saada mote ara!"> 
         </form>
-
     </body>
 </html>
