a247d8ce90c96d1928b51549ba72627aa7fad90c
Lisanne Järv <lisannejarv@gmail.com>
2020-09-22 08:28:45 +0300

    Update home.php

--- home.php
+++ home.php
@@ -105,12 +105,6 @@
   <hr>
   <?php echo $imghtml ; ?>
   
-  <hr>
-    <form method = "POST">
-      <label>Sisesta oma pahe tulnud mote!</label>
-      <input type="text" name="ideainput" placeholder="Kirjuta siia mote!">
-      <input type="submit" name="ideasubmit" value="Saada mote ara!"> 
-    </form>
-  <hr>
+  
 </body>
 </html>
