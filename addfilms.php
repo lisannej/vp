a7af5b32ae6bafc6069dcb8cb5e3b13499f9cfe0
Lisanne Järv <lisannejarv@gmail.com>
2020-09-22 11:38:10 +0300

    Update addfilms.php

--- addfilms.php
+++ addfilms.php
@@ -18,7 +18,7 @@ if(isset($_POST["filmsubmit"])){
      $inputerror .= "Ebareaalne valmimisaasta!";
    }
    if(empty($inputerror)){
-      savefilm ($_POST ["titleinput"], $_POST ["yearinput"], $_POST ["durationinput"], $_POST ["genreinput"], $_POST ["studioinput"], $_POST ["directorinput"] )
+      savefilm ($_POST ["titleinput"], $_POST ["yearinput"], $_POST ["durationinput"], $_POST ["genreinput"], $_POST ["studioinput"], $_POST ["directorinput"] );
    }
   }
 
