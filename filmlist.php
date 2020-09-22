fcb465ff77b80181cba01cbd3bece4453fdb75b8
Lisanne Järv <lisannejarv@gmail.com>
2020-09-22 09:36:55 +0300

    tund4

--- filmlist.php
+++ filmlist.php
@@ -1,30 +1,12 @@
 <?php
 require ("header.php");
 require ("config.php");
+require ("fnc_films.php");
+
 $database = "if20_lisanne_ja_1" ;
 //loen lehele koik olemasolevad motted
-$conn = new mysqli ($serverhost, $serverusername, $serverpassword, $database );
-//$stmt = $conn->prepare ("SELECT pealkiri, aasta, kestus, zanr, tootja, lavastaja FROM film");
-$stmt = $conn->prepare ("SELECT * FROM film");
-echo $conn->error;
-//seome tulemuse muutujaga
-$stmt->bind_result ($titlefromdb, $yearfromdb, $durationfromdb, $genrefromdb, $studiofromdb, $directorfromdb);
-$stmt->execute ();
-$filmhtml = "<ol> \n";
-while ($stmt->fetch ()) {
-    $filmhtml .= "\t \t <li>".$titlefromdb ."\n";
-    $filmhtml .= "\t \t \t <ul> \n";
-    $filmhtml .= "\t \t \t \t <li>Valmimisaasta: " .$yearfromdb ."</li> \n";
-    $filmhtml .= "\t \t \t \t <li>Kestus minutites: " .$durationfromdb ." minutit</li> \n";
-    $filmhtml .= "\t \t \t \t <li>Zanr: " .$genrefromdb ."</li> \n";
-    $filmhtml .= "\t \t \t \t <li>Tootja: " .$studiofromdb ."</li> \n";
-    $filmhtml .= "\t \t \t \t <li>Lavastaja: " .$directorfromdb ."</li> \n";
-    $filmhtml .= "\t \t \t </ul> \n";
-    $filmhtml .= "\t \t </li> \n";
-    $filmhtml .= "\t </ol> \n";
-}
-$stmt->close ();
-  $conn->close ();
+readfilms ();
+
 ?>
 
 
---
+++ fnc_films.php
@@ -0,0 +1,29 @@
+<?php
+$database = "if20_lisanne_ja_1" ;
+
+//funktsionn mis loeb koikide filmide info
+function readfilms (){
+    $conn = new mysqli ($serverhost, $serverusername, $serverpassword, $database );
+    //$stmt = $conn->prepare ("SELECT pealkiri, aasta, kestus, zanr, tootja, lavastaja FROM film");
+    $stmt = $conn->prepare ("SELECT * FROM film");
+    echo $conn->error;
+    //seome tulemuse muutujaga
+    $stmt->bind_result ($titlefromdb, $yearfromdb, $durationfromdb, $genrefromdb, $studiofromdb, $directorfromdb);
+    $stmt->execute ();
+    $filmhtml = "<ol> \n";
+    while ($stmt->fetch ()) {
+        $filmhtml .= "\t \t <li>".$titlefromdb ."\n";
+        $filmhtml .= "\t \t \t <ul> \n";
+        $filmhtml .= "\t \t \t \t <li>Valmimisaasta: " .$yearfromdb ."</li> \n";
+        $filmhtml .= "\t \t \t \t <li>Kestus minutites: " .$durationfromdb ." minutit</li> \n";
+        $filmhtml .= "\t \t \t \t <li>Zanr: " .$genrefromdb ."</li> \n";
+        $filmhtml .= "\t \t \t \t <li>Tootja: " .$studiofromdb ."</li> \n";
+        $filmhtml .= "\t \t \t \t <li>Lavastaja: " .$directorfromdb ."</li> \n";
+        $filmhtml .= "\t \t \t </ul> \n";
+        $filmhtml .= "\t \t </li> \n";
+        $filmhtml .= "\t </ol> \n";
+    }
+    $stmt->close ();
+      $conn->close ();
+} //readfilms loppeb
+?>
