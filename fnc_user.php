7371c3d63683873ec1056cead4e4ee0d05629b0a
Lisanne Järv <lisannejarv@gmail.com>
2020-09-29 11:29:31 +0300

    uue kasutaja loomine

--- fnc_user.php
+++ fnc_user.php
@@ -23,3 +23,34 @@ function signup($firstnameinput, $lastnameinput, $emailinput, $genderinput, $bir
     return $result;
 }
 
+function signin ($emailinput, $passwordinput) {
+    $result = null;
+    $conn = new mysqli ($GLOBALS ["serverhost"], $GLOBALS ["serverusername"], $GLOBALS ["serverpassword"], $GLOBALS ["database"] );
+    $stmt = $conn->prepare("SELECT password FROM vpusers WHERE email = ?");
+    echo $conn->error;
+    $stmt->bind_param("s", $emailinput);
+    $stmt->bind_result ($passwordfromdb);
+    if($stmt->execute()){
+        //kui tehniliselt korras
+        if($stmt->fetch()){
+            //kasutaja leiti
+            if(password_verify($passwordinput, $passwordfromdb)){
+                //parool oige
+                $stmt->close();
+                $conn->close();
+                header("Location: home.php");
+                exit();
+            } else {
+                $result = "Vale salasona";
+            }
+        } else {
+            $result = "Sellist kasutajat (" .$emailinput .") ei leitud";
+        }
+    } else {
+        //tehniline viga
+        $result= $stmt->error;
+    }
+    $stmt->close();
+    $conn->close();
+    return $result;
+}
