c635628beaea79ed829159ba230bb5ccbbab3445
Lisanne Järv <lisannejarv@gmail.com>
2020-10-02 21:52:58 +0300

    Update page.php

--- page.php
+++ page.php
@@ -1,5 +1,4 @@
 <?php
-  require ("header.php");
   require ("config.php");
   require ("fnc_common.php");
   require ("fnc_user.php");
@@ -220,81 +219,88 @@
   //}
 ?>
 
-<img src="IMG/vp_banner.png" alt="Veebiprogrammeerimise kursuse banner">
-<p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
-<p> See konkreetne leht on loodud veebiprogrammeerimise kursusel aasta 2020 sügissemestril <a href="https://www.tlu.ee">Tallinna Ülikooli </a> 
-Digitehnoloogiate instituudis.</p>
-<p>Lehe avamise hetk: <?php echo $weekdaynameset [$weekdaynow -1 ],", ".$daydate, ", ".$monthnameset [$monthnowint],", ".$yearnow,", " .$fulltimenow; ?>.</p>
-<p><?php echo "Praegu on " .$partofday ."."; ?></p>
-<p><?php echo "Praegu on möödunud semestri algusest " .$daysnumber ." päeva, mis on " .$semesterpercent ." %"; ?><p>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <?php $username = "Lisanne Järv"; ?>
+  <title><?php echo $username; ?> programmeerib veebi</title>
+</head>
+<body>
+  <img src="IMG/vp_banner.png" alt="Veebiprogrammeerimise kursuse banner">
+  <p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p> See konkreetne leht on loodud veebiprogrammeerimise kursusel aasta 2020 sügissemestril <a href="https://www.tlu.ee">Tallinna Ülikooli </a> 
+  Digitehnoloogiate instituudis.</p>
+  <p>Lehe avamise hetk: <?php echo $weekdaynameset [$weekdaynow -1 ],", ".$daydate, ", ".$monthnameset [$monthnowint],", ".$yearnow,", " .$fulltimenow; ?>.</p>
+  <p><?php echo "Praegu on " .$partofday ."."; ?></p>
+  <p><?php echo "Praegu on möödunud semestri algusest " .$daysnumber ." päeva, mis on " .$semesterpercent ." %"; ?><p>
 
-<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  <label for="firstnameinput"> Eesnimi </label>
-  <input type="text" name="firstnameinput" id="firstname" placeholder="Eesnimi" value="<?php echo $firstnameinput; ?>"><span><?php echo $firstnameinputerror; ?></span>
-  <br>
-  <label for="lastnameinput"> Perekonnanimi </label>
-  <input type="text" name="lastnameinput" id="lastname" placeholder="Perekonnanimi" value="<?php echo $lastnameinput; ?>"><span><?php echo $lastnameinputerror; ?></span>
-  <br> 
-  <input type="radio" name="genderinput" id="gendermale" value="1" <?php if($genderinput == "1"){echo " checked";}?>><label for="gendermale">Mees</label>
-  <input type="radio" name="genderinput" id="genderfemale" value="2" <?php if($genderinput == "2"){echo " checked";}?>><label for="genderfemale">Naine</label>
-  <span><?php echo $genderinputerror; ?></span>
-  <br>
-  <label for="birthdayinput">Sünnipäev: </label>
-  <?php
-    echo '<select name="birthdayinput" id="birthdayinput">' ."\n";
-    echo "\t \t" .'<option value="" selected disabled>päev</option>' ."\n";
-    for ($i = 1; $i < 32; $i ++){
-      echo "\t" .'<option value="' .$i .'"';
-      if ($i == $birthday){
-        echo " selected ";
+  <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <label for="firstnameinput"> Eesnimi </label>
+    <input type="text" name="firstnameinput" id="firstname" placeholder="Eesnimi" value="<?php echo $firstnameinput; ?>"><span><?php echo $firstnameinputerror; ?></span>
+    <br>
+    <label for="lastnameinput"> Perekonnanimi </label>
+    <input type="text" name="lastnameinput" id="lastname" placeholder="Perekonnanimi" value="<?php echo $lastnameinput; ?>"><span><?php echo $lastnameinputerror; ?></span>
+    <br> 
+    <input type="radio" name="genderinput" id="gendermale" value="1" <?php if($genderinput == "1"){echo " checked";}?>><label for="gendermale">Mees</label>
+    <input type="radio" name="genderinput" id="genderfemale" value="2" <?php if($genderinput == "2"){echo " checked";}?>><label for="genderfemale">Naine</label>
+    <span><?php echo $genderinputerror; ?></span>
+    <br>
+    <label for="birthdayinput">Sünnipäev: </label>
+    <?php
+      echo '<select name="birthdayinput" id="birthdayinput">' ."\n";
+      echo "\t \t" .'<option value="" selected disabled>päev</option>' ."\n";
+      for ($i = 1; $i < 32; $i ++){
+        echo "\t" .'<option value="' .$i .'"';
+        if ($i == $birthday){
+          echo " selected ";
+        }
+        echo ">" .$i ."</option> \n";
       }
-      echo ">" .$i ."</option> \n";
-    }
-    echo  "</select> \n";
+      echo  "</select> \n";
+      ?>
+    <label for="birthmonthinput">Sünnikuu: </label>
+    <?php
+      echo '<select name="birthmonthinput" id="birthmonthinput">' ."\n";
+      echo "\t \t" .'<option value="" selected disabled>kuu</option>' ."\n";
+      for ($i = 1; $i < 13; $i ++){
+        echo '<option value="' .$i .'"';
+        if ($i == $birthmonth){
+          echo " selected ";
+        }
+        echo ">" .$monthnameset[$i - 1] ."</option> \n";
+      }
+      echo " \t </select> \n";
     ?>
-  <label for="birthmonthinput">Sünnikuu: </label>
-  <?php
-    echo '<select name="birthmonthinput" id="birthmonthinput">' ."\n";
-    echo "\t \t" .'<option value="" selected disabled>kuu</option>' ."\n";
-    for ($i = 1; $i < 13; $i ++){
-      echo '<option value="' .$i .'"';
-      if ($i == $birthmonth){
-        echo " selected ";
+    <label for="birthyearinput">Sünniaasta: </label>
+    <?php
+      echo "\t" .'<select name="birthyearinput" id="birthyearinput">' ."\n";
+      echo "\t \t" .'<option value="" selected disabled>aasta</option>' ."\n";
+      for ($i = date("Y") - 15; $i >= date("Y") - 110; $i --){
+        echo '<option value="' .$i .'"';
+        if ($i == $birthyear){
+          echo " selected ";
+        }
+        echo ">" .$i ."</option> \n";
       }
-      echo ">" .$monthnameset[$i - 1] ."</option> \n";
-    }
-    echo " \t </select> \n";
-  ?>
-  <label for="birthyearinput">Sünniaasta: </label>
-  <?php
-    echo "\t" .'<select name="birthyearinput" id="birthyearinput">' ."\n";
-    echo "\t \t" .'<option value="" selected disabled>aasta</option>' ."\n";
-    for ($i = date("Y") - 15; $i >= date("Y") - 110; $i --){
-      echo '<option value="' .$i .'"';
-      if ($i == $birthyear){
-        echo " selected ";
-      }
-      echo ">" .$i ."</option> \n";
-    }
-    echo "\t </select> \n";
-  ?>
-  <br>
-  <br>
-  <span><?php echo $birthdateerror ." " .$birthdayerror ." " .$birthmontherror ." " .$birthyearerror; ?></span>
-  <label for="emailinput"> Email (kasutajatunnus)</label>
-  <input type="email" name="emailinput" id="email" placeholder="Email" value="<?php echo $emailinput; ?>"><span><?php echo $emailinputerror; ?></span>
-  <br>
-  <label for="passwordinput"> Salasona (min 8 tahemarki) </label>
-  <input type="password" name="passwordinput" id="password" placeholder="Salasona" value="<?php echo $passwordinput; ?>"><span><?php echo $passwordinputerror; ?></span>
-  <br>
-  <label for="passwordsecondaryinput"> Salasona uuesti</label>
-  <input type="password" name="passwordsecondaryinput" id="passwordsecondary" placeholder="Salasona uuesti" value="<?php echo $passwordsecondaryinput; ?>"><span><?php echo $passwordsecondaryinputerror; ?></span>
-  <br>
-  <input type="submit" name="userinput" value="Loo kasutaja"><br><span><?php echo $result; ?></span>
-</form>
-<?php echo $inputerror ; ?>
-<?php echo $imghtml ; ?>
-  
+      echo "\t </select> \n";
+    ?>
+    <br>
+    <br>
+    <span><?php echo $birthdateerror ." " .$birthdayerror ." " .$birthmontherror ." " .$birthyearerror; ?></span>
+    <label for="emailinput"> Email (kasutajatunnus)</label>
+    <input type="email" name="emailinput" id="email" placeholder="Email" value="<?php echo $emailinput; ?>"><span><?php echo $emailinputerror; ?></span>
+    <br>
+    <label for="passwordinput"> Salasona (min 8 tahemarki) </label>
+    <input type="password" name="passwordinput" id="password" placeholder="Salasona" value="<?php echo $passwordinput; ?>"><span><?php echo $passwordinputerror; ?></span>
+    <br>
+    <label for="passwordsecondaryinput"> Salasona uuesti</label>
+    <input type="password" name="passwordsecondaryinput" id="passwordsecondary" placeholder="Salasona uuesti" value="<?php echo $passwordsecondaryinput; ?>"><span><?php echo $passwordsecondaryinputerror; ?></span>
+    <br>
+    <input type="submit" name="userinput" value="Loo kasutaja"><br><span><?php echo $result; ?></span>
+  </form>
+  <?php echo $inputerror ; ?>
+  <?php echo $imghtml ; ?>
   
 </body>
 </html>
