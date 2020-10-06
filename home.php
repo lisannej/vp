50ae5f3ace72ec88477a2fb93857e80a3bc50080
Lisanne Järv <lisannejarv@gmail.com>
2020-10-06 11:00:34 +0300

    usesession loodud

--- home.php
+++ home.php
@@ -1,19 +1,8 @@
 <?php
 session_start();
 
-  //kas on sessioon olemas
-  if(!isset($_SESSION["userid"])){
-    //jouga suunatake sisselogimise lehele
-    header("Location: page.php");
-    exit();
-  }
-  //LOGIME VALJA
-  if(isset($_GET["logout"])){
-    session_destroy();
-    header("Location:page.php");
-    exit();
-  }
-  
+
+
   //annan ette lubatud piltivormingute loendi
   $picfiletypes = ["image/jpeg", "image/png"];
   // loeme piltide kataloogi sisu ja naitame pilte
@@ -42,14 +31,16 @@ session_start();
   $imghtml .= 'alt="Tallinna Ulikool">';
   //}
   require ("header.php");
+  require ("usesession.php")
 ?>
 
   <img src="IMG/vp_banner.png" alt="Veebiprogrammeerimise kursuse banner">
   <h1><?php echo $_SESSION["userfirstname"] ." " .$_SESSION["userlastname"]; ?></h1>
+  <p><a href="home.php/logout=1">Logi valja</a></li>
   <p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
   <p> See konkreetne leht on loodud veebiprogrammeerimise kursusel aasta 2020 sügissemestril <a href="https://www.tlu.ee">Tallinna Ülikooli </a> 
   Digitehnoloogiate instituudis.</p>
-  <p><a href="home.php/logout=1">Logi valja</a></li>
+  
   
   
   <?php echo $imghtml ; ?>
---
+++ usesession.php
@@ -0,0 +1,14 @@
+<?php
+  //kas on sessioon olemas
+  if(!isset($_SESSION["userid"])){
+    //jouga suunatake sisselogimise lehele
+    header("Location: page.php");
+    exit();
+  }
+  //LOGIME VALJA
+  if(isset($_GET["logout"])){
+    session_destroy();
+    header("Location:page.php");
+    exit();
+  }
+  
