ad4f5ff9489c934c2b0940e89091f149c9a2d567
Lisanne Järv <lisannejarv@gmail.com>
2020-10-06 09:59:57 +0300

    Update header.php

--- header.php
+++ header.php
@@ -10,12 +10,12 @@
     if(isset($_SESSION["userbgcolor"])){
       echo "\t \t background-color: " .$_SESSION["userbgcolor"] ." \n";
     } else {
-      echo "\t \t background-color: " #FFFFFF; ." \n";
+      echo "\t \t background-color:  #FFFFFF;  \n";
     }
     if(isset($_SESSION["usertxtcolor"])){
       echo "\t \t color: " .$_SESSION["usertxtcolor"] ." \n";
     } else {
-      echo "\t \t color: " #000000; ." \n";
+      echo "\t \t color:  #000000; \n";
     }
   ?>
   </style>
