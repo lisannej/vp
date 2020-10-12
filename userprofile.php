049b45b2c3aca5f9c0881c989d7bb788080f6b59
Lisanne Järv <lisannejarv@gmail.com>
2020-10-12 13:02:04 +0300

    Update userprofile.php

--- userprofile.php
+++ userprofile.php
@@ -18,38 +18,32 @@
     $notice = storeuserprofile($userdescription, $_POST["bgcolorinput"], $_POST["txtcolorinput"]);
     $_SESSION["userbgcolor"] = $_POST["bgcolorinput"];
     $_SESSION["usertxtcolor"] = $_POST["txtcolorinput"];
-    }
-   
-    require("header.php");
-  ?>
-    <img src="../img/vp_banner.png" alt="Veebiprogrammeerimise kursuse bänner">
-    <h1><?php echo $_SESSION["userfirstname"] ." " .$_SESSION["userlastname"]; ?></h1>
-    <p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
-    <p>See konkreetne leht on loodud veebiprogrammeerimise kursusel aasta 2020 sügissemestril <a href="https://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
-    
-    <ul>
-    <li><a href="?logout=1">Logi välja</a>!</li>
-      <li><a href="home.php">Avaleht</a></li>
-    <li><a href="listfilms.php">Vaata sisestatud filme</a></li>
-    </ul>
-    
-    <hr>
-    
-    <form method="POST" action:"<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ;?>">
-    <label for="descriptioninput">Minu lühikirjeldus</label>
-    <br>
-    <textarea rows="10" cols="80" name="descriptioninput" id="descriptioninput" placeholder="Minu lühikirjeldus..."><?php echo $userdescription; ?></textarea>
-    <br>
-    <label for="bgcolorinput">Minu valitud taustavärv</label>
-    <input type="color" name="bgcolorinput" id="bgcolorinput" value="<?php echo $_SESSION["userbgcolor"]; ?>">
-    <br>
-    <label for="txtcolorinput">Minu valitud tekstivärv</label>
-    <input type="color" name="txtcolorinput" id="txtcolorinput" value="<?php echo $_SESSION["usertxtcolor"]; ?>">
-    <br>
-    
-    <input type="submit" name="profilesubmit" value="Salvesta profiil">
-    </form>
-    <p><?php echo $notice; ?></p>
+  }
+  require ("header.php");
+?>
+
+
+<img src="IMG/vp_banner.png" alt="Veebiprogrammeerimise kursuse banner">
+  <h1><?php echo $_SESSION["userfirstname"] ." " .$_SESSION["userlastname"]; ?></h1>
+  <p>See konkreetne veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p> See konkreetne leht on loodud veebiprogrammeerimise kursusel aasta 2020 sügissemestril <a href="https://www.tlu.ee">Tallinna Ülikooli </a> 
+  Digitehnoloogiate instituudis.</p>
+
+<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+  <label for="descriptioninput"> Minu luhikirjeldus </label>
+  <br>
+  <textarea rows="10" cols="80" name="descriptioninput" id="descriptioninput" placeholder="Minu luhikirjeldus..."><?php echo $userdescription ?></textarea>
+  <br>
+	<label for="bgcolorinput">Minu valitud taustavärv: </label>
+	<input type="color" name="bgcolorinput" id="bgcolorinput" value="<?php echo $_SESSION["userbgcolor"]; ?>">
+	<br>
+	<label for="txtcolorinput">Minu valitud tekstivärv: </label>
+	<input type="color" name="txtcolorinput" id="txtcolorinput" value="<?php echo $_SESSION["usertxtcolor"]; ?>">
+	<br>
+  <input type="submit" name="profilesubmit" value="Salvesta profiil">
+</form>
+<?php echo $notice ?>
+>>>>>>> parent of 097b7df... Update userprofile.php
 
 </body>
 </html>
