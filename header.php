dd191e432a00fb497d6a26b6a572a93ed5016531
Lisanne Järv <lisannejarv@gmail.com>
2020-10-12 12:20:38 +0300

    Update header.php

--- header.php
+++ header.php
@@ -7,17 +7,17 @@
   <stlye>
     <?php
       echo "body { \n";
-      if(isset($_SESSION["userbgcolor"])){
-        echo "\t background-color: " .$_SESSION["userbgcolor"] ."; \n";
-      } else {
-        echo "\t background-color: #FFFFFF;  \n";
-      }
-      if(isset($_SESSION["usertxtcolor"])){
-        echo "\t color: " .$_SESSION["usertxtcolor"] ."; \n";
-      } else {
-        echo "\t color: #000000; \n";
-      }
-      echo "\t } \n";
+        if(isset($_SESSION["userbgcolor"])){
+          echo "\t background-color: " .$_SESSION["userbgcolor"] ."; \n";
+        } else {
+          echo "\t background-color: #FFFFFF; \n";
+        }
+        if(isset($_SESSION["usertxtcolor"])){
+          echo "\t color: " .$_SESSION["usertxtcolor"] ."; \n";
+        } else {
+          echo "\t color: #000000; \n";
+        }
+        echo "\t } \n";
     ?>
   </style>
 </head>
