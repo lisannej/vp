11be54adc4d7df61e1c01a5f7402c3e48e10b686
Lisanne Järv <lisannejarv@gmail.com>
2020-10-12 17:05:50 +0300

    filmiseos

--- addfilms.php
+++ addfilms.php
@@ -2,6 +2,7 @@
 require ("header.php");
 require ("config.php");
 require ("fnc_films.php");
+require ("usesession.php");
 
 $inputerror=""; 
 //$database="if20_lisanne_ja_1" ;
@@ -27,7 +28,7 @@ if(isset($_POST["filmsubmit"])){
 
 
 <img src="IMG/vp_banner.png" alt="Veebiprogrammeerimise kursuse banner">
-  <h1><?php echo $username; ?></h1>
+<h1><?php echo $_SESSION["userfirstname"] ." " .$_SESSION["userlastname"]; ?></h1>
   <p>See konkreetne veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
   <p> See konkreetne leht on loodud veebiprogrammeerimise kursusel aasta 2020 sügissemestril <a href="https://www.tlu.ee">Tallinna Ülikooli </a> 
   Digitehnoloogiate instituudis.</p>
--- header.php
+++ header.php
@@ -32,6 +32,7 @@
       <td><a href="addfilms.php">Lisa filme</a></td>
       <td><a href="addnewuser.php">Tee kasutaja</a></td>
       <td><a href="userprofile.php">Minu kasutajaprofiil</a></td>
+      <td><a href="filmiseos.php">Loo filmiseoseid</a></td>
     </tr>
   </table>
 
