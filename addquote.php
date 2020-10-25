40bc9c7062fa3a12f380c6098d9b1e974ee431cd
Lisanne Järv <lisannejarv@gmail.com>
2020-10-25 22:46:05 +0200

    new

---
+++ addquote.php
@@ -0,0 +1,43 @@
+<?php
+require ("usesession.php");
+require ("header.php");
+require ("config.php");
+require ("fnc_films.php");
+
+
+$inputerror=""; 
+//$database="if20_lisanne_ja_1" ;
+
+//loen lehele koik olemasolevad tegelased
+//$personhtml=readpeople();
+//kui klikiti submit siis
+if(isset($_POST["quotesubmit"])){
+  echo"tegutsen";
+    if(empty($_POST["quoteinput"])){
+        $inputerror .="Osa infot on sisestamata! ";
+    }
+    if(empty($inputerror)){
+        echo"salvestan";
+        savequote($_POST["quoteinput"]);
+    }
+}
+
+?>
+
+
+<img src="IMG/vp_banner.png" alt="Veebiprogrammeerimise kursuse banner">
+<h1><?php echo $_SESSION["userfirstname"] ." " .$_SESSION["userlastname"]; ?></h1>
+  <p>See konkreetne veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p> See konkreetne leht on loodud veebiprogrammeerimise kursusel aasta 2020 sügissemestril <a href="https://www.tlu.ee">Tallinna Ülikooli </a> 
+  Digitehnoloogiate instituudis.</p>
+
+<form method="POST">
+  <label for="quoteinput"> Lisa tsitaat </label>
+  <input type="text" name="quoteinput" id="quoteinput" placeholder="Tsitaat" >
+  <br>
+  <input type="submit" name="quotesubmit" value="Salvesta tegelase info">
+</form>
+<?php echo $inputerror ?>
+
+</body>
+</html>
