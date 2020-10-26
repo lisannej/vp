d8f83ddfa7c4877fb1263ca8f0d49978456be8b1
Lisanne Järv <lisannejarv@gmail.com>
2020-10-26 15:51:35 +0200

    Update header.php

--- header.php
+++ header.php
@@ -23,17 +23,16 @@
  <body> 
   <table>
     <tr>
-      <td><a href="page.php">ESILEHT</a></td>
       <td><a href="home.php">Kodu</a></td>
-      <td><a href="mote.php">Mote</a></td>
-      <td><a href="motetabel.php">Tabel</a></td>
+      <td><a href="mote.php">Mõte</a></td>
+      <td><a href="motetabel.php">Mõtete tabel</a></td>
       <td><a href="filmlist.php">Filmlist</a></td>
       <td><a href="addfilms.php">Lisa filme</a></td>
       <td><a href="addperson.php">Lisa tegelasi</a></td>
+      <td><a href="addquote.php">Lisa tsitaate</a></td>
       <td><a href="addfilmrelationgenre.php">Loo filmiseoseid</a></td>
       <td><a href="listfilmpersons.php">Filmitegelased</a></td>
       <td><a href="listquotes.php">Filmi tsitaadid</a></td>
-      <td><a href="addnewuser.php">Tee kasutaja</a></td>
       <td><a href="userprofile.php">Minu kasutajaprofiil</a></td>
 
     </tr>
