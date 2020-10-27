082e6503ade81d6ec9aa9320accca97507de7f2e
Lisanne Järv <lisannejarv@gmail.com>
2020-10-27 11:05:31 +0200

    Update photoupload.php

--- photoupload.php
+++ photoupload.php
@@ -4,11 +4,15 @@ require ("header.php");
 require ("config.php");
 
 $inputerror=""; 
+$notice= null;
 $filetype=null;
 $filesizelimit=1048576;
 $photouploaddir_orig= "photoupload_orig/";
+$photouploaddir_normal= "photoupload_normal/";
 $filenameprefix= "vp_";
 $filename= null;
+$photomaxwidth= 600;
+$phtotmaxheight=400;
 
 //kui klikiti submit siis
 if(isset($_POST["photosubmit"])){
@@ -16,7 +20,7 @@ if(isset($_POST["photosubmit"])){
     //var_dump($_FILES);
     $check = getimagesize(($_FILES["photoinput"]["tmp_name"]));
     if($check!== false){
-        if($check["mime"]== "image/jpeg"){
+        if($check["mime"]== "image/jpg"){
             $filetype="jpg";
         }
         if($check["mime"]== "image/png"){
@@ -41,10 +45,62 @@ if(isset($_POST["photosubmit"])){
         $inputerror="Selle nimega fail on juba olemas! ";
     }
         if(empty($inputerror)){
+            //muudame suurust
+            //loome pikslikogumi, pildi objekti
+            if($filetype=="jpg"){
+                $mytempimage = imagecreatefromjpeg($_FILES["photoinput"]["tmp_name"]);
+            }
+            if($filetype=="png"){
+                $mytempimage = imagecreatefrompng($_FILES["photoinput"]["tmp_name"]);
+            }
+            if($filetype=="gif"){
+                $mytempimage = imagecreatefromgif($_FILES["photoinput"]["tmp_name"]);
+            }
+        
+            //teeme kindlaks originaalsuuruse
+            $imagew = imagesx($mytempimage);
+            $imageh = imagesy($mytempimage);
+            if($imagew > $photomaxwidth or $imageh > $photomaxheight){
+                if($imagew / $photomaxwidth > $imageh / $photomaxheight){
+                    $photosizeratio = $imagew / $photomaxwidth;
+                } else {
+                    $photosizeratio = $imageh / $photomaxheight;
+                }
+            
+                //arvutame uued moodud
+                $neww = round($imagew / $photosizeratio);
+                $newh = round($imageh / $photosizeratio);
+                //teeme uue pikslikogumi
+                $mynewtempimage = imagecreatetruecolor($neww, $newh);
+                //kirjutame jarelejaavad pikslid uuele pildile
+                imagecopyresampled($mynewtempimage, $mytempimage, 0, 0, 0, 0, $neww, $newh, $imagew, $imageh);
+                //salvestame faili
+                if($filetype=="jpg"){
+                    if(imagejpeg($mynewtempimage, $photouploaddir_normal .$filename, 90)){
+                        $notice = "Vahendatud pildi salvestamine onnestus!";
+                    } else {
+                        $notice = "Vahendatud pildi salvestamisel tekkis viga";
+                    }
+                if($filetype=="png"){
+                    if(imagepng($mynewtempimage, $photouploaddir_normal .$filename, 6)){
+                        $notice = "Vahendatud pildi salvestamine onnestus!";
+                    } else {
+                        $notice = "Vahendatud pildi salvestamisel tekkis viga";
+                    }
+                if($filetype=="gif"){
+                    if(imagegif($mynewtempimage, $photouploaddir_normal .$filename)){
+                        $notice = "Vahendatud pildi salvestamine onnestus!";
+                    } else {
+                        $notice = "Vahendatud pildi salvestamisel tekkis viga";
+                    }
+                }
+            
+            }
         move_uploaded_file($_FILES["photoinput"]["tmp_name"], $photouploaddir_orig .$filename);
         }
+            }
     }
-
+}
 ?>
 
 
@@ -72,7 +128,9 @@ if(isset($_POST["photosubmit"])){
   <br>
   <input type="submit" name="photosubmit" value="Lae foto ules">
 </form>
-<?php echo $inputerror ?>
+<?php 
+    echo $inputerror; 
+    echo $notice; ?>
 
 </body>
 </html>
