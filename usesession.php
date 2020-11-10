de0eec61bd1503ccea0f915eca4b64abbfbb3290
Lisanne Järv <lisannejarv@gmail.com>
2020-11-10 09:38:28 +0200

    Update usesession.php

--- usesession.php
+++ usesession.php
@@ -1,6 +1,6 @@
 <?php
   //session_start();
-  require ("classes/sessionmanager_class.php");
+  require ("sessionmanager_class.php");
   SessionManager::sessionStart("vp20", 0, "/~lisajar/", "greeny.cs.tlu.ee" );
 
   //kas on sessioon olemas
