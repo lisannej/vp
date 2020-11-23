905bfe5e68628dfd651cd83e66c17a75ba47f298
Lisanne Järv <lisannejarv@gmail.com>
2020-11-23 21:39:02 +0200

    Update photoupload.php

--- photoupload.php
+++ photoupload.php
@@ -1,7 +1,8 @@
 <?php
   
-  require ("sessionmanager_class.php");
-  SessionManager::sessionStart("vp20", 0, "/~lisajar/", "greeny.cs.tlu.ee" );
+  //require ("sessionmanager_class.php");
+  //SessionManager::sessionStart("vp20", 0, "/~lisajar/", "greeny.cs.tlu.ee" );
+  require ("usesession.php"); 
   require ("config.php");
   require ("fnc_photo.php");
   require ("fnc_common.php");
@@ -25,6 +26,94 @@
     
   //kui klikiti submit, siis ...
   if(isset($_POST["photosubmit"])){
+	$privacy = intval($_POST["privinput"]);
+	$alttext = test_input($_POST["altinput"]);
+	//var_dump($_POST);
+	//var_dump($_FILES);
+	//kas on pilt ja mis tüüpi
+	$check = getimagesize($_FILES["photoinput"]["tmp_name"]);
+	if($check !== false){
+		//var_dump($check);
+		if($check["mime"] == "image/jpeg"){
+			$filetype = "jpg";
+		}
+		if($check["mime"] == "image/png"){
+			$filetype = "png";
+		}
+		if($check["mime"] == "image/gif"){
+			$filetype = "gif";
+		}
+	} else {
+		$inputerror = "Valitud fail ei ole pilt! ";
+	}
+	
+	//kas on sobiva failisuurusega
+	if(empty($inputerror) and $_FILES["photoinput"]["size"] > $filesizelimit){
+		$inputerror = "Liiga suur fail!";
+	}
+	
+	//loome uue failinime
+	$timestamp = microtime(1) * 10000;
+	$filename = $filenameprefix .$timestamp ."." .$filetype;
+	
+	//ega fail äkki olemas pole
+	if(file_exists($photouploaddir_orig .$filename)){
+		$inputerror = "Selle nimega fail on juba olemas!";
+	}
+	
+	//kui vigu pole ...
+	if(empty($inputerror)){
+		
+		//võtame kasutusele klassi
+		$myphoto = new Photoupload($_FILES["photoinput"], $filetype);
+		//teeme pildi väiksemaks
+		$myphoto->resizePhoto($photomaxwidth, $photomaxheight, true);
+		//lisame vesimärgi
+		$myphoto->addWatermark($watermark);
+		//salvestame vähendatud pildi
+		$result = $myphoto->saveimage($photouploaddir_normal .$filename);
+		if($result == 1){
+			$notice .= " Vähendatud pildi salvestamine õnnestus!";
+		} else {
+			$inputerror .= " Vähendatud pildi salvestamisel tekkis tõrge!";
+		}
+		
+		//teeme pisipildi
+		$myphoto->resizePhoto($thumbsize, $thumbsize);
+		$result = $myphoto->saveimage($photouploaddir_thumb .$filename);
+		if($result == 1){
+			$notice .= " Pisipildi salvestamine õnnestus!";
+		} else {
+			$inputerror .= "Pisipildi salvestamisel tekkis tõrge!";
+		}
+		//eemaldan klassi
+		unset($myphoto);
+		
+		//salvestame originaalpildi
+		if(empty($inputerror)){
+			if(move_uploaded_file($_FILES["photoinput"]["tmp_name"], $photouploaddir_orig .$filename)){
+				$notice .= " Originaalfaili üleslaadimine õnnestus!";
+			} else {
+				$inputerror .= " Originaalfaili üleslaadimisel tekkis tõrge!";
+			}
+		}
+		
+		if(empty($inputerror)){
+			$result = storePhotoData($filename, $alttext, $privacy);
+			if($result == 1){
+				$notice .= " Pildi info lisati andmebaasi!";
+				$privacy = 1;
+				$alttext = null;
+			} else {
+				$inputerror .= "Pildi info andmebaasi salvestamisel tekkis tõrge!";
+			}
+		} else {
+			$inputerror .= " Tekkinud vigade tõttu pildi andmeid ei salvestatud!";
+		}
+		
+	}
+  }
+  /*if(isset($_POST["photosubmit"])){
 	//var_dump($_POST);
 	//var_dump($_FILES);
 	//kas on pilt ja mis tüüpi
@@ -100,6 +189,7 @@
 		$inputerror .= " Tekkinud vigade tõttu pildi andmeid ei salvestatud!";
 	}
   }
+  */
   
   require("header.php");
 ?>
