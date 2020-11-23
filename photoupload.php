d0002e4b2ad4e67a6d6914fe62d049de693bd032
Lisanne Järv <lisannejarv@gmail.com>
2020-11-23 21:43:14 +0200

    Update photoupload.php

--- photoupload.php
+++ photoupload.php
@@ -1,6 +1,6 @@
 <?php
   require ("usesession.php");
-  require ("sessionmanager_class.php");
+  //require ("sessionmanager_class.php");
   //SessionManager::sessionStart("vp20", 0, "/~lisajar/", "greeny.cs.tlu.ee" );
    
   require ("config.php");
