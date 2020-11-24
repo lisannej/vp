72b8918415309a7c51a95be54c177aca05c13db1
Lisanne Järv <lisannejarv@gmail.com>
2020-11-24 08:13:36 +0200

    Update usesession.php

--- usesession.php
+++ usesession.php
@@ -1,7 +1,7 @@
 <?php
   //session_start();
   require ("sessionmanager_class.php");
-  SessionManager::sessionStart("vp", 0, "/~lisajar/", "greeny.cs.tlu.ee");
+  SessionManager::sessionStart("vp20", 0, "/~lisajar/", "greeny.cs.tlu.ee");
   
   //kas on sisse loginud
   if(!isset($_SESSION["userid"])){
