55e7a0e7db5ecfc08d4c4b98b7c61cc786eff30a
Lisanne Järv <lisannejarv@gmail.com>
2020-11-24 08:16:53 +0200

    Update userprofile.php

--- userprofile.php
+++ userprofile.php
@@ -1,18 +1,5 @@
 <?php
-  session_start();
-
-   //kui pole sisseloginud
-   if(!isset($_SESSION["userid"])){
- 	  //jõugu sisselogimise lehele
- 	  header("Location: page.php");
-   }
-   //väljalogimine
-   if(isset($_GET["logout"])){
- 	  session_destroy();
- 	   header("Location: page.php");
- 	   exit();
-   }
-
+  require ("usesession.php");
    //loeme andmebaasi login info muutujad
    require ("config.php");
    require ("fnc_user.php");
