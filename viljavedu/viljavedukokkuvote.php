45faec76e4190a91e2e29a8cd19fd7af6c14d290
Lisanne Järv <lisannejarv@gmail.com>
2020-12-12 21:44:34 +0200

    Update viljavedukokkuvote.php

--- viljavedu/viljavedukokkuvote.php
+++ viljavedu/viljavedukokkuvote.php
@@ -28,8 +28,8 @@
         $notice .= "\n\t\t\t" .'<th>Auto registreerimisnumber &nbsp;<a href="?filmsortby=2&filmsortorder=1">&uarr;</a>&nbsp;<a href="?filmsortby=2&filmsortorder=2">&darr;</a></th>';
         $notice .= "\n\t\t\t" .'<th>Sisseveo mass &nbsp;<a href="?filmsortby=3&filmsortorder=1">&uarr;</a>&nbsp;<a href="?filmsortby=3&filmsortorder=2">&darr;</a></th>';
         $notice .= "\n\t\t\t" .'<th>Valjumismass &nbsp;<a href="?filmsortby=1&filmsortorder=1">&uarr;</a>&nbsp;<a href="?filmsortby=1&filmsortorder=2">&darr;</a></th>';
-        $notice .= "\t</tr>\n\t" .$lines ."</table>\n";
-        $notice.= $lines;
+        $notice .= "\t</tr>\n\t" .$carhtml ."</table>\n";
+        $notice.= $carhtml;
         $notice.= "</table> \n";
     }
     $stmt->close ();
