8d465c2c0f71281f42342d889b86e17ab00a86f3
Lisanne Järv <lisannejarv@gmail.com>
2020-12-12 22:07:14 +0200

    Update viljavedukokkuvote.php

--- viljavedu/viljavedukokkuvote.php
+++ viljavedu/viljavedukokkuvote.php
@@ -9,6 +9,7 @@
     $notice;
     $sortby=0;
     $sortorder=0;
+
     function carloads (){
         $conn = new mysqli ($GLOBALS ["serverhost"], $GLOBALS ["serverusername"], $GLOBALS ["serverpassword"], $GLOBALS ["database"] );
         //$stmt = $conn->prepare ("SELECT pealkiri, aasta, kestus, zanr, tootja, lavastaja FROM film");
@@ -29,38 +30,35 @@
             $stmt = $conn->prepare($SQLsentence ." ORDER BY valjumismass DESC"); 
           }
         }
+        //loen lehele koik olemasolevad motted
+        $conn = new mysqli ($serverhost, $serverusername, $serverpassword, $database );
+        $stmt = $conn->prepare ("SELECT auto_reg_number, sisenemismass, valjumismass FROM viljavedu");
+        echo $conn->error;
+        //seome tulemuse muutujaga
+        $stmt->bind_result ($carfromdb, $entermass, $exitmass);
+        $stmt->execute ();
+        $carhtml = "";
+        while ($stmt->fetch ()) {
+            $carhtml .= "<tr> \n";
+            $carhtml .= "\t <td>" .$carfromdb ."</td>";
+            $carhtml .= "\t <td>" .$entermass ."</td>";
+            $carhtml .= "\t <td>" .$exitmass ."</td>";
+            $carhtml .= "</tr> \n";
+        }
+        if(!empty($carhtml)){
+            $notice = "<table> \n" ;
+            $notice.= "<tr> \n";
+            $notice .= "\n\t\t\t" .'<th>Auto registreerimisnumber &nbsp;<a href="?filmsortby=2&filmsortorder=1">&uarr;</a>&nbsp;<a href="?filmsortby=2&filmsortorder=2">&darr;</a></th>';
+            $notice .= "\n\t\t\t" .'<th>Sisseveo mass &nbsp;<a href="?filmsortby=3&filmsortorder=1">&uarr;</a>&nbsp;<a href="?filmsortby=3&filmsortorder=2">&darr;</a></th>';
+            $notice .= "\n\t\t\t" .'<th>Valjumismass &nbsp;<a href="?filmsortby=1&filmsortorder=1">&uarr;</a>&nbsp;<a href="?filmsortby=1&filmsortorder=2">&darr;</a></th>';
+            //$notice .= "\t</tr>\n\t" .$carhtml ."</table>\n";
+            $notice.= $carhtml;
+            $notice.= "</table> \n";
+        }
+        $stmt->close ();
+        $conn->close ();
+        return $notice;
     }
-    //loen lehele koik olemasolevad motted
-    $conn = new mysqli ($serverhost, $serverusername, $serverpassword, $database );
-    $stmt = $conn->prepare ("SELECT auto_reg_number, sisenemismass, valjumismass FROM viljavedu");
-    echo $conn->error;
-    //seome tulemuse muutujaga
-    $stmt->bind_result ($carfromdb, $entermass, $exitmass);
-    $stmt->execute ();
-    $carhtml = "";
-    while ($stmt->fetch ()) {
-        $carhtml .= "<tr> \n";
-        $carhtml .= "\t <td>" .$carfromdb ."</td>";
-        $carhtml .= "\t <td>" .$entermass ."</td>";
-        $carhtml .= "\t <td>" .$exitmass ."</td>";
-        $carhtml .= "</tr> \n";
-    }
-    if(!empty($carhtml)){
-        $notice = "<table> \n" ;
-        $notice.= "<tr> \n";
-        $notice .= "\n\t\t\t" .'<th>Auto registreerimisnumber &nbsp;<a href="?filmsortby=2&filmsortorder=1">&uarr;</a>&nbsp;<a href="?filmsortby=2&filmsortorder=2">&darr;</a></th>';
-        $notice .= "\n\t\t\t" .'<th>Sisseveo mass &nbsp;<a href="?filmsortby=3&filmsortorder=1">&uarr;</a>&nbsp;<a href="?filmsortby=3&filmsortorder=2">&darr;</a></th>';
-        $notice .= "\n\t\t\t" .'<th>Valjumismass &nbsp;<a href="?filmsortby=1&filmsortorder=1">&uarr;</a>&nbsp;<a href="?filmsortby=1&filmsortorder=2">&darr;</a></th>';
-        //$notice .= "\t</tr>\n\t" .$carhtml ."</table>\n";
-        $notice.= $carhtml;
-        $notice.= "</table> \n";
-    }
-    $stmt->close ();
-    $conn->close ();
-
-    $notice = carloads($sortby, $sortorder);
-    
-
     if(isset($_GET["sortby"])and isset($_GET["sortorder"])){
         if($_GET["sortby"]>= 1 and $_GET["sortby"]<= 4){
             $sortby=$_GET["sortby"];
@@ -70,6 +68,6 @@
         }
     }
     //echo readquotes($sortby, $sortorder);
-    echo $notice;
+    echo carloads($sortby, $sortorder);
 
 ?>
