1d9fc55fcca090a14f3e1a7bc9679b94e13dd70b
Lisanne Järv <lisannejarv@gmail.com>
2020-12-12 20:57:08 +0200

    Update lisaviljavedu.php

--- viljavedu/lisaviljavedu.php
+++ viljavedu/lisaviljavedu.php
@@ -29,10 +29,10 @@
     <input type="text" name="carnumberinput" id="carnumberinput" placeholder="Auto number" >
     <br>
     <label for="fullloadinput"> Lisa auto sisenemismass</label>
-    <input type="text" name="fullloadinput" id="fullloadinput" placeholder="Auto sisenemismass" >
+    <input type="number" name="fullloadinput" id="fullloadinput" placeholder="Auto sisenemismass" >
     <br>
     <label for="emptyloadinput"> Lisa auto valjumismass</label>
-    <input type="text" name="emptyloadinput" id="emptyloadinput" placeholder="Auto sisenemismass" >
+    <input type="number" name="emptyloadinput" id="emptyloadinput" placeholder="Auto sisenemismass" >
     <br>
     <input type="submit" name="datasubmit" value="Salvesta andmed">
   </form>
