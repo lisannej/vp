fc930308c65a308546e33bf5ee796743de473a46
Lisanne Järv <lisannejarv@gmail.com>
2020-12-12 23:27:37 +0200

    Update viljavedukokkuvote.php

--- viljavedu/viljavedukokkuvote.php
+++ viljavedu/viljavedukokkuvote.php
@@ -10,48 +10,16 @@
     $exitmass;
     $notice;
 
-    if(isset($_GET["sortby"])and isset($_GET["sortorder"])){
-        if($_GET["sortby"]>= 1 and $_GET["sortby"]<= 4){
-            $sortby=$_GET["sortby"];
-        }
-        if($_GET["sortorder"]==1 or $_GET["sortorder"]){
-            $sortorder=$_GET["sortorder"];
-        }
+    if(isset($_GET["filter"])){
+        $SQLsentence= ('SELECT auto_reg_number, sisenemismass, valjumismass FROM viljavedu WHERE auto_reg_number = $_GET["filter"]');
+        $stmt = $conn->prepare($SQLsentence);
     }
 
     $conn = new mysqli ($GLOBALS ["serverhost"], $GLOBALS ["serverusername"], $GLOBALS ["serverpassword"], $database);
-    //$stmt = $conn->prepare ("SELECT pealkiri, aasta, kestus, zanr, tootja, lavastaja FROM film");
-    $SQLsentence= ("SELECT auto_reg_number, sisenemismass, valjumismass FROM viljavedu ");
-    if($sortby == 0 and $sortorder == 0) {
-        $stmt = $conn->prepare($SQLsentence);
-    }
-    if($sortby == 1) {
-        if($sortorder == 2) {
-        $stmt = $conn->prepare($SQLsentence ." ORDER BY auto_reg_number DESC"); 
-        }
-        else {
-        $stmt = $conn->prepare($SQLsentence ." ORDER BY auto_reg_number"); 
-        }
-    }
-    if($sortby == 2) {
-        if($sortorder == 2) {
-        $stmt = $conn->prepare($SQLsentence ." ORDER BY sisenemismass DESC"); 
-        }
-        else {
-        $stmt = $conn->prepare($SQLsentence ." ORDER BY sisenemismass"); 
-        }
-    }
-    if($sortby == 3) {
-        if($sortorder == 2) {
-            $stmt = $conn->prepare($SQLsentence ." ORDER BY valjumismass DESC"); 
-        }
-        else {
-            $stmt = $conn->prepare($SQLsentence ." ORDER BY valjumismass"); 
-        }
-    }
+    
     //loen lehele koik olemasolevad motted
-    //$conn = new mysqli ($serverhost, $serverusername, $serverpassword, $database );
-    //$stmt = $conn->prepare ("SELECT auto_reg_number, sisenemismass, valjumismass FROM viljavedu");
+    $conn = new mysqli ($serverhost, $serverusername, $serverpassword, $database );
+    $stmt = $conn->prepare ("SELECT auto_reg_number, sisenemismass, valjumismass FROM viljavedu");
     echo $conn->error;
     //seome tulemuse muutujaga
     $stmt->bind_result ($carfromdb, $entermass, $exitmass);
@@ -67,9 +35,9 @@
     if(!empty($carhtml)){
         $notice = "<table> \n" ;
         $notice.= "<tr> \n";
-        $notice .= "\n\t\t\t" .'<th>Auto registreerimisnumber &nbsp;<a href="?filmsortby=1&filmsortorder=1">&uarr;</a>&nbsp;<a href="?filmsortby=1&filmsortorder=2">&darr;</a></th>';
-        $notice .= "\n\t\t\t" .'<th>Sisseveo mass &nbsp;<a href="?filmsortby=2&filmsortorder=1">&uarr;</a>&nbsp;<a href="?filmsortby=2&filmsortorder=2">&darr;</a></th>';
-        $notice .= "\n\t\t\t" .'<th>Valjumismass &nbsp;<a href="?filmsortby=3&filmsortorder=1">&uarr;</a>&nbsp;<a href="?filmsortby=3&filmsortorder=2">&darr;</a></th>';
+        $notice .= "\n\t\t\t" .'<th>Auto registreerimisnumber</th>';
+        $notice .= "\n\t\t\t" .'<th>Sisseveo mass</th>';
+        $notice .= "\n\t\t\t" .'<th>Valjumismass</th>';
         $notice.= $carhtml;
         $notice.= "</tr> \n";
         $notice.= "</table> \n";
@@ -78,7 +46,10 @@
     $conn->close ();
     
     echo $notice;
-    //echo carloads(2, 2);
-    //echo carloads($sortby, $sortorder);
 
 ?>
+
+<form method="POST">
+    <label for="filter"> Vali auto </label>
+    <input type="text" name="carinput" id="filter" placeholder="Auto reg number" >
+</form>
