82d172f9561dcb1861c7b4ef4d4df33553a744e1
Lisanne Järv <lisannejarv@gmail.com>
2020-12-12 23:32:37 +0200

    Update viljavedukokkuvote.php

--- viljavedu/viljavedukokkuvote.php
+++ viljavedu/viljavedukokkuvote.php
@@ -8,7 +8,7 @@
     $exitmass;
     $notice;
 
-    if(isset($_GET["filter"])){
+    if(isset($_POST["datasubmit"]) and isset($_GET["filter"])){
         $SQLsentence= "SELECT auto_reg_number, sisenemismass, valjumismass FROM viljavedu WHERE auto_reg_number = " .$_GET["filter"];
         $stmt = $conn->prepare($SQLsentence);
     }
@@ -50,4 +50,5 @@
 <form method="POST">
     <label for="filter"> Vali auto </label>
     <input type="text" name="carinput" id="filter" placeholder="Auto reg number" >
+    <input type="submit" name="datasubmit" value="Salvesta filter">
 </form>
