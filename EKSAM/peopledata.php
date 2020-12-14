241d13457a203afb406e9533a966e0eaa65dbfe6
Lisanne Järv <lisannejarv@gmail.com>
2020-12-14 12:48:32 +0200

    Update peopledata.php

--- EKSAM/peopledata.php
+++ EKSAM/peopledata.php
@@ -4,38 +4,79 @@
 
     $database = "if20_lisanne_ja_1";
     $genderfromdb;
+    $maleteacherfromdb;
+    $malestudentfromdb;
+    $femalestudentfromdb;
+    $femaleteacherfromdb;
+
     $entryexitfromdb;
     $occupationfromdb;
     $notice;
     $SQLsentence="";
 
     $conn = new mysqli ($GLOBALS ["serverhost"], $GLOBALS ["serverusername"], $GLOBALS ["serverpassword"], $database);
-    $stmt = $conn->prepare ("SELECT count(entryexit), gender, occupation FROM inimesed WHERE entryexit=5");
+    $stmt = $conn->prepare ("SELECT count(entryexit) FROM inimesed WHERE entryexit=5");
     echo $conn->error;
     //seome tulemuse muutujaga
-    $stmt->bind_result ($entryexitfromdb, $genderfromdb, $occupationfromdb);
+    $stmt->bind_result ($entryexitfromdb);
     $stmt->execute ();
-    $lines = "";
+    $entryexithtml = "";
     while ($stmt->fetch ()) {
-        $lines .= "<tr> \n";
-        $lines .= "\t <td>" .$entryexitfromdb ."</td>";
-        $lines .= "\t <td>" .$genderfromdb ."</td>";
-        $lines .= "\t <td>" .$occupationfromdb ."</td>";
-        $lines .= "</tr> \n";
-    }
-    if(!empty($lines)){
-        $notice = "<table> \n" ;
-        $notice.= "<tr> \n";
-        $notice .= "\n\t\t\t" .'<th>Inimesi hoones sees</th>';
-        $notice .= "\n\t\t\t" .'<th>Sugu</th>';
-        $notice .= "\n\t\t\t" .'<th>Ala</th>';
-        $notice.= $lines;
-        $notice.= "</tr> \n";
-        $notice.= "</table> \n";
+        $entryexithtml .= "<p>" .$entryexitfromdb ."</p>";
     }
     $stmt->close ();
     $conn->close ();
-    
+
+    $stmt = $conn->prepare ("SELECT count(gender) FROM inimesed WHERE gender=1 AND occupation=3");
+    echo $conn->error;
+    //seome tulemuse muutujaga
+    $stmt->bind_result ($genderfromdb);
+    $stmt->execute ();
+    $malestudenthtml = "";
+    while ($stmt->fetch ()) {
+        $malestudenthtml .= "<p>" .$malestudentfromdb ."</p>";
+    }
+    $stmt->close ();
+    $conn->close ();
+
+    $stmt= $conn->prepare ("SELECT count(gender) FROM inimesed WHERE gender=1 AND occupation=4");
+    echo $conn->error;
+    //seome tulemuse muutujaga
+    $stmt->bind_result ($genderfromdb);
+    $stmt->execute ();
+    $maleteacherhtml = "";
+    while ($stmt->fetch ()) {
+        $maleteacherhtml .= "<p>" .$maleteacherfromdb ."</p>";
+    }
+    $stmt->close ();
+    $conn->close ();
+
+    $stmt= $conn->prepare ("SELECT count(gender) FROM inimesed WHERE gender=2 AND occupation=3");
+    echo $conn->error;
+    //seome tulemuse muutujaga
+    $stmt->bind_result ($genderfromdb);
+    $stmt->execute ();
+    $femalestudenthtml = "";
+    while ($stmt->fetch ()) {
+        $femalestudenthtml .= "<p>" .$femalestudentfromdb ."</p>";
+    }
+    $stmt->close ();
+    $conn->close ();
+
+    $stmt= $conn->prepare ("SELECT count(gender) FROM inimesed WHERE gender=2 AND occupation=4");
+    echo $conn->error;
+    //seome tulemuse muutujaga
+    $stmt->bind_result ($genderfromdb);
+    $stmt->execute ();
+    $femaleteacherhtml = "";
+    while ($stmt->fetch ()) {
+        $femaleteacherhtml .= "<p>" .$femaleteacherfromdb ."</p>";
+    }
+    $stmt->close ();
+    $conn->close ();
+
     echo $notice;
 
 ?>
+<p> Meessoost õpilasi hoones: <?php echo $malestudenthtml?> <p>
+<p> Meessoost õpetajaid hoones: <p>
