c08433259dc4b4f62318b4bba864da3c551072e9
Lisanne Järv <lisannejarv@gmail.com>
2020-12-14 13:59:20 +0200

    Update peopledata.php

--- EKSAM/peopledata.php
+++ EKSAM/peopledata.php
@@ -14,58 +14,58 @@
 
 
     $conn = new mysqli ($GLOBALS ["serverhost"], $GLOBALS ["serverusername"], $GLOBALS ["serverpassword"], $database);
-    $stmt = $conn->prepare ("SELECT count(entryexit) FROM inimesed WHERE entryexit=5");
-    echo $conn->error;
-    //seome tulemuse muutujaga
-    $stmt->bind_result ($entryexitfromdb);
-    $stmt->execute ();
-    $entryexithtml = "";
-    while ($stmt->fetch ()) {
-        $entryexithtml .= "<p> Inimesi kokku hoones: " .$entryexitfromdb ."</p>";
-    }
-        $stmt->close ();
-    
+        $stmt = $conn->prepare ("SELECT count(entryexit) FROM inimesed WHERE entryexit=5");
+        echo $conn->error;
+        //seome tulemuse muutujaga
+        $stmt->bind_result ($entryexitfromdb);
+        $stmt->execute ();
+        $entryexithtml = "";
+        while ($stmt->fetch ()) {
+            $entryexithtml .= "<p> Inimesi kokku hoones: " .$entryexitfromdb ."</p>";
+        }
+            $stmt->close ();
+        
 
-    $stmt = $conn->prepare ("SELECT count(gender) FROM inimesed WHERE gender=1 AND occupation=3");
-    echo $conn->error;
-    echo "ma siin";
-    $stmt->bind_result ($genderfromdb);
-    $stmt->execute ();
-    echo "jama siin";
-    $malestudenthtml = "";
-    while ($stmt->fetch ()) {
-        $malestudenthtml .= "<p> Meessoost õpilasi hoones: " .$malestudentfromdb ."</p>";
-    } echo "lopus";
-        $stmt->close ();
-    
-    $stmt= $conn->prepare ("SELECT Count(gender) FROM inimesed WHERE gender=1 AND occupation=4");
-    echo $conn->error;
-    $stmt->bind_result ($genderfromdb);
-    $stmt->execute ();
-    $maleteacherhtml = "";
-    while ($stmt->fetch ()) {
-        $maleteacherhtml .= "<p> Meessoost õpetajaid hoones: " .$maleteacherfromdb ."</p>";
-    }
-    //$stmt->close ();
+        $stmt = $conn->prepare ("SELECT count(gender) FROM inimesed WHERE gender=1 AND occupation=3");
+        echo $conn->error;
+        echo "ma siin";
+        $stmt->bind_result ($genderfromdb);
+        $stmt->execute ();
+        echo "jama siin";
+        $malestudenthtml = "";
+        while ($stmt->fetch ()) {
+            $malestudenthtml .= "<p> Meessoost õpilasi hoones: " .$malestudentfromdb ."</p>";
+        } echo "lopus";
+            $stmt->close ();
+        
+        $stmt= $conn->prepare ("SELECT Count(gender) FROM inimesed WHERE gender=1 AND occupation=4");
+        echo $conn->error;
+        $stmt->bind_result ($genderfromdb);
+        $stmt->execute ();
+        $maleteacherhtml = "";
+        while ($stmt->fetch ()) {
+            $maleteacherhtml .= "<p> Meessoost õpetajaid hoones: " .$maleteacherfromdb ."</p>";
+        }
+        //$stmt->close ();
 
-    $stmt= $conn->prepare ("SELECT count(gender) FROM inimesed WHERE gender=2 AND occupation=3");
-    echo $conn->error;
-    $stmt->bind_result ($genderfromdb);
-    $stmt->execute ();
-    $femalestudenthtml = "";
-    while ($stmt->fetch ()) {
-        $femalestudenthtml .= "<p> Naissoost õpilasi hoones: " .$femalestudentfromdb ."</p>";
-    }
-    //$stmt->close ();
+        $stmt= $conn->prepare ("SELECT count(gender) FROM inimesed WHERE gender=2 AND occupation=3");
+        echo $conn->error;
+        $stmt->bind_result ($genderfromdb);
+        $stmt->execute ();
+        $femalestudenthtml = "";
+        while ($stmt->fetch ()) {
+            $femalestudenthtml .= "<p> Naissoost õpilasi hoones: " .$femalestudentfromdb ."</p>";
+        }
+        //$stmt->close ();
 
-    $stmt= $conn->prepare ("SELECT count(gender) FROM inimesed WHERE gender=2 AND occupation=4");
-    echo $conn->error;
-    $stmt->bind_result ($genderfromdb);
-    $stmt->execute ();
-    $femaleteacherhtml = "";
-    while ($stmt->fetch ()) {
-        $femaleteacherhtml .= "<p> Naissoost õpetajaid hoones: " .$femaleteacherfromdb ."</p>";
-    }
+        $stmt= $conn->prepare ("SELECT count(gender) FROM inimesed WHERE gender=2 AND occupation=4");
+        echo $conn->error;
+        $stmt->bind_result ($genderfromdb);
+        $stmt->execute ();
+        $femaleteacherhtml = "";
+        while ($stmt->fetch ()) {
+            $femaleteacherhtml .= "<p> Naissoost õpetajaid hoones: " .$femaleteacherfromdb ."</p>";
+        }
     $stmt->close ();
     $conn->close ();
 
