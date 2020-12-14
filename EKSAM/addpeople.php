270a393234687e2e9f5322e971cae3f41f4f8085
Lisanne Järv <lisannejarv@gmail.com>
2020-12-14 10:56:44 +0200

    Update addpeople.php

--- EKSAM/addpeople.php
+++ EKSAM/addpeople.php
@@ -22,9 +22,12 @@ if(isset($_POST["datasubmit"]) and !empty($_POST["datasubmit"])){
 ?>
 
 <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<label for="genderinput"> Kas siseneja on mees/naine? </label>
 <input type="radio" name="genderinput" id="gendermale" value="1" <?php if($genderinput == "1"){echo " checked";}?>><label for="gendermale">Mees</label>
 <input type="radio" name="genderinput" id="genderfemale" value="2" <?php if($genderinput == "2"){echo " checked";}?>><label for="genderfemale">Naine</label>
 <span><?php echo $genderinputerror; ?></span>
+<br>
+<label for="occupationinput"> Kas ta on õpilane või õpetaja? </label>
 <br>
 <input type="radio" name="occupationinput" id="student" value="3" <?php if($occupationinput == "3"){echo " checked";}?>><label for="occupationstudent">Õpilane</label>
 <input type="radio" name="occupationinput" id="teacher" value="4" <?php if($occupationinput == "4"){echo " checked";}?>><label for="occupationteacher">Õpetaja</label>
